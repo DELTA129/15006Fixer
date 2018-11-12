e272457739ec42f96d8bb5fa14848e0552fc4335
Martin Hrehor <mato.hrehor@gmail.com>
2018-11-12 22:41:43 +0100

    Initial commit

---
+++ 15006Fixer/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace _15006Fixer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.serverSelect = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.buttonRevert = new System.Windows.Forms.Button();
+            this.buttonApply = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // serverSelect
+            // 
+            this.serverSelect.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.serverSelect.FormattingEnabled = true;
+            this.serverSelect.Location = new System.Drawing.Point(73, 45);
+            this.serverSelect.Name = "serverSelect";
+            this.serverSelect.Size = new System.Drawing.Size(148, 24);
+            this.serverSelect.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Server:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Status:";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(73, 13);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(81, 17);
+            this.statusLabel.TabIndex = 3;
+            this.statusLabel.Text = "statusLabel";
+            // 
+            // buttonRevert
+            // 
+            this.buttonRevert.Location = new System.Drawing.Point(349, 32);
+            this.buttonRevert.Name = "buttonRevert";
+            this.buttonRevert.Size = new System.Drawing.Size(125, 37);
+            this.buttonRevert.TabIndex = 4;
+            this.buttonRevert.Text = "Revert changes";
+            this.buttonRevert.UseVisualStyleBackColor = true;
+            this.buttonRevert.Click += new System.EventHandler(this.buttonRevert_Click);
+            // 
+            // buttonApply
+            // 
+            this.buttonApply.Location = new System.Drawing.Point(240, 32);
+            this.buttonApply.Name = "buttonApply";
+            this.buttonApply.Size = new System.Drawing.Size(103, 37);
+            this.buttonApply.TabIndex = 5;
+            this.buttonApply.Text = "Apply";
+            this.buttonApply.UseVisualStyleBackColor = true;
+            this.buttonApply.Click += new System.EventHandler(this.buttonApply_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(486, 187);
+            this.label3.TabIndex = 6;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 281);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonApply);
+            this.Controls.Add(this.buttonRevert);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.serverSelect);
+            this.Name = "Form1";
+            this.Text = "SC 15006 Fixer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox serverSelect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Button buttonRevert;
+        private System.Windows.Forms.Button buttonApply;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ 15006Fixer/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _15006Fixer
+{
+    public partial class Form1 : Form
+    {
+        string hostname = "public.universe.robertsspaceindustries.com";
+        string fixerNote = "#Following line added by SC 15006 Fixer:";
+        string hostsPath = "C:/Windows/System32/drivers/etc/hosts";
+        Dictionary<string, string> servers = new Dictionary<string, string> { { "US 1", "50.17.105.39" }, { "US 2", "18.215.191.91" }, { "US 3", "100.24.145.206" }, { "US 4", "52.204.2.76" }, { "Asia 1", "13.237.27.15" }, { "Asia 2", "52.62.175.64" } };
+
+        public Form1()
+        {
+            InitializeComponent();
+            serverSelect.DataSource = new List<string>(servers.Keys);
+            serverSelect.DropDownStyle = ComboBoxStyle.DropDownList;
+            RefreshState();
+        }
+        void RefreshState()
+        {
+            if (File.Exists(hostsPath))
+            {
+                string[] lines = File.ReadAllLines(hostsPath);
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (lines[i] == fixerNote)
+                    {
+                        string[] entry = lines[i + 1].Split(' ');
+                        foreach (var v in servers)
+                        {
+                            if (entry[0] == v.Value)
+                            {
+                                statusLabel.Text = "Fix applied, rerouted to: " + v.Key;
+                                return;
+                            }
+                        }
+                        statusLabel.Text = "Fix applied, rerouted to: " + "[failed to determine]";
+                        return;
+                    }
+                }
+                statusLabel.Text = "No fix applied";
+            }
+            else statusLabel.Text = "Hosts file not found";
+        }
+        private void buttonApply_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(hostsPath))
+            {
+                MessageBox.Show("It seems that your PC does not have a hosts file. This program will add it, but it is likely that this fix won't work.");
+                File.Create(hostsPath);
+            }
+
+            List<string> lines = new List<string>(File.ReadAllLines(hostsPath));
+            bool foundExisting = false;
+            string entry = servers[serverSelect.Text] + ' ' + hostname + "\r\n";
+            for (int i = 0; i < lines.Count; i++)
+            {
+                if (lines[i] == fixerNote)
+                {
+                    lines[i + 1] = entry;
+                    foundExisting = true;
+                }
+            }
+            if (!foundExisting)
+            {
+                lines.Add(fixerNote);
+                lines.Add(entry);
+            }
+            File.WriteAllLines(hostsPath, lines, Encoding.ASCII);
+            MessageBox.Show("Applied. Be sure to revert any changes if the fix does not work! Restarting your computer might help if the fix is not effective straight away.");
+            RefreshState();
+        }
+
+        private void buttonRevert_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(hostsPath))
+            {
+                MessageBox.Show("Hosts file does not exist. There's nothing to revert.");
+            }
+            else
+            {
+
+                List<String> lines = new List<string>(File.ReadAllLines(hostsPath));
+                int entryIndex = -1;
+                for (int i = 0; i < lines.Count; i++)
+                {
+                    if (lines[i] == fixerNote)
+                    {
+                        entryIndex = i;
+                    }
+                }
+                if (entryIndex != -1)
+                {
+                    lines.RemoveAt(entryIndex+1);
+                    lines.RemoveAt(entryIndex);
+                    File.WriteAllLines(hostsPath, lines, Encoding.ASCII);
+                    MessageBox.Show("All changes reverted successfully.");
+                }
+                else
+                {
+                    MessageBox.Show("Hosts file does not contain any changes. There's nothing to revert!");
+                }
+            }
+            RefreshState();
+        }
+    }
+}
